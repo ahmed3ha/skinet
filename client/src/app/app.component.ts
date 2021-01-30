9203427784e1f0703b45007b940f65b2f55470d7
ahmed3ha <ahmedts@hotmail.com>
2021-01-30 10:51:01 +0200

    End of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 
 
@@ -10,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Skinet';
-  constructor(){}
+  constructor(private basketservices: BasketService){}
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketservices.getBasket(basketId).subscribe(() =>
+      {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
-
 }
