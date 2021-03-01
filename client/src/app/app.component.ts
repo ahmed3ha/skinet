fd1f45a99325e6602a1778f2e64bedfd93371cb1
ahmed3ha <ahmedts@hotmail.com>
2021-03-01 13:29:46 +0200

    End of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,47 @@
-
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
-
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Skinet';
-  constructor(private basketservices: BasketService){}
+  constructor(
+    private basketservices: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+
+    this.accountService.loadCurrentUser(token).subscribe(
+      () => {
+        console.log('loaded user');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
-      this.basketservices.getBasket(basketId).subscribe(() =>
-      {
-        console.log('initialised basket');
-      }, error => {
-        console.log(error);
-      });
+      this.basketservices.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialised basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 }
--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -1,23 +1,29 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { delay, finalize } from "rxjs/operators";
-import { BusyService } from "../services/busy.service";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { delay, finalize } from 'rxjs/operators';
+import { BusyService } from '../services/busy.service';
 
 @Injectable()
-export class LoadingInterceptor implements HttpInterceptor{
-    constructor(private BusyService: BusyService)
-    {
-
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private busyService: BusyService) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (!req.url.includes('emailexists')) {
+      this.busyService.busy();
     }
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.BusyService.busy();
-        return next.handle(req).pipe(
-            delay(1000),
-            finalize(()=>{
-                this.BusyService.idle();
-            })
-        );
-    }
-
+    return next.handle(req).pipe(
+      delay(1000),
+      finalize(() => {
+        this.busyService.idle();
+      })
+    );
+  }
 }
